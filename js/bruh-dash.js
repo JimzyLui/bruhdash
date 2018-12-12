ed17d532dba5e5db3121090db96f1d8e2a1b511f
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-11 23:27:23 -1000

    snapshot

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -8,133 +8,101 @@ var global = window || GLOBAL;
  ****************************************************************************************/
 
 global.bruhdash = {
-
   // returns the first element of an array
-  first: function () {
-      
+  first: function(arr) {
+    return arr.shift();
   },
 
   // returns the last element of an array
-  last: function () {
-
+  last: function(arr) {
+    return arr.pop();
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function () {
-
+  indexOf: function(arr, str) {
+    return arr.indexOf(str);
   },
 
   // returns the index of the first matching element from right to left
-  lastIndexOf: function () {
-
+  lastIndexOf: function(arr, str) {
+    return arr.lastIndexOf(str);
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
-
+  initial: function(arr) {
+    arr.splice(-1, 1);
+    return arr;
   },
-  
-  // returns an array with all falsey values removed
-  compact: function() {
 
+  // returns an array with all falsey values removed
+  compact: function(arr) {
+    return arr.filter(x => Boolean(x) === true);
   },
 
   // creates a slice of an array from the start index up to but not including the end index
-  slice: function () {
-
+  slice: function(arr, start, end) {
+    return arr.slice(start, end);
   },
 
   // returns a slice of array with n elements dropped from the beignning
-  drop: function(){
-
-  },
+  drop: function() {},
 
   // returns a slice of array with n elements dropped from the end
-  dropRight: function() {
-
-  },
+  dropRight: function() {},
 
   // creates a slice of an array with n elements taken from the beginning
-  take: function () {
-
-  },
+  take: function() {},
 
   // creates a slice of an array with n elements taken from the end
-  takeRight: function () {
-
-  },
+  takeRight: function() {},
 
   // fills elements of array with specified value from the start index
   // up to but not including the end index
-  fill: function() {
-
-  },
+  fill: function() {},
 
   // removes all given values from an array
-  pull: function () {
-
-  },
+  pull: function() {},
 
   // removes elements of an array corresponding to the given indices
-  pullAt: function () {
-
-  },
+  pullAt: function() {},
 
   // creates an array excluding all the specified values
-  without: function() {
-
-  },
+  without: function() {},
 
   // returns an array with specified values excluded
-  difference: function() {
-
-  },
+  difference: function() {},
 
   /*******************
    *  STRETCH GOALS! *
-   *******************/ 
+   *******************/
 
   // creates an array of grouped elements
-  zip: function () {
-
-  },
+  zip: function() {},
 
   // creates an array of grouped elements in their pre-zip configuration
-  unzip: function () {
-
-  },
+  unzip: function() {},
 
   // creates an array of elements into groups of length of specified size
-  chunk: function(){
-
-  },
+  chunk: function() {},
 
   // iterates over elements of a collection and invokes iteratee for each element
   // Note: this should work for arrays and objects
-  forEach: function() {
-
-  },
+  forEach: function() {},
 
   // creates an array of values by running each element in collection thru the iteratee
   // Note: this should work for arrays and objects
-  map: function() {
-
-  },
+  map: function() {},
 
   /*************************
    *  SUPER STRETCH GOALS!  *
-   *************************/ 
+   *************************/
 
   // iterates over elements of a collection and returns all elements that the predicate returns truthy for
   // Note: this should work for arrays and objects
-  filter: function() {
-
-  },
+  filter: function() {},
 
   // Reduces the collection to a value which is the accumulated result of running each element
   // in the collection through an iteratee
   // Note: this should work for arrays and objects
-  reduce: function() {
-    
-  }
+  reduce: function() {}
 };
